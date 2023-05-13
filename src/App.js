7b85d6c1334f9ce58eae8b9db23d84edf3a92850
Diogo Paes <Diogo.1hotmail.com>
2023-05-13 12:18:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { HomeScreen, ListScreen } from "./screens";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/list" element={<ListScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
